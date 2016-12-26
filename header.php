414685927ff398f6908ae53d89a1fe14e613311b
Sabuj Das <sabujdas94@gmail.com>
2016-12-26 19:20:07 +0600

    Add Main menu icon property

--- header.php
+++ header.php
@@ -21,58 +21,40 @@ global $cot_opt;
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'cot' ); ?></a>
+    <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'cot' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php /* TODO remove this cod with proper code
-			 if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif; */ ?>
-                        <div class="navbar" role="navigation">
-                            <div class="container">
-                                <div class="navbar-header">
-                                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#top-navbar-1">
-                                        <span class="sr-only">Toggle navigation</span>
-                                        <span class="icon-bar"></span>
-                                        <span class="icon-bar"></span>
-                                        <span class="icon-bar"></span>
-                                    </button>
-                                    <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">
-                                        <img src="<?php echo esc_url($cot_opt['cot-logo']['url']);?>" alt="<?php bloginfo( 'name' );?>">
-                                    </a>
-                                </div>
-                                <!-- Collect the nav links, forms, and other content for toggling -->
-                                <?php
-                                    wp_nav_menu( array(
-                                        'menu'              => 'primary',
-                                        'theme_location'    => 'primary',
-                                        'depth'             => 2,
-                                        'container'         => 'div',
-                                        'container_class'   => 'collapse navbar-collapse',
-                                        'container_id'      => 'top-navbar-1',
-                                        'menu_class'        => 'nav navbar-nav navbar-right',
-                                        'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-                                        'walker'            => new wp_bootstrap_navwalker())
-                                    );
-                                ?>
-                            </div>
-                        </div>
+    <header id="masthead" class="site-header" role="banner">
+        <div class="site-branding">
+            <div class="navbar" role="navigation">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#top-navbar-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>">
+                            <img src="<?php echo esc_url($cot_opt['cot-logo']['url']);?>" alt="<?php bloginfo( 'name' );?>">
+                        </a>
+                    </div>
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <?php
+                        wp_nav_menu( array(
+                            'menu'              => 'primary',
+                            'theme_location'    => 'primary',
+                            'depth'             => 2,
+                            'container'         => 'div',
+                            'container_class'   => 'collapse navbar-collapse',
+                            'container_id'      => 'top-navbar-1',
+                            'menu_class'        => 'nav navbar-nav navbar-right',
+                            'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+                            'walker'            => new wp_bootstrap_navwalker())
+                        );
+                    ?>
+                </div>
+            </div>
+        </div><!-- .site-branding -->
+    </header><!-- #masthead -->
 
-			<?php $description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
-		<?php /*<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'cot' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation --> */?>
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
+    <div id="content" class="site-content">
